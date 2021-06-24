343511e0a2e28a727a0ceb37a7f081cb84d965d3
Karen <lopk.karen@gmail.com>
2021-06-25 11:03:40 +1200

    add select func db.js

---
+++ server/db/db.js
@@ -0,0 +1,13 @@
+const environment = process.env.NODE_ENV || 'development'
+const config = require('./knexfile')[environment]
+const connection = require('knex')(config)
+
+module.exports = {
+  getAnswer
+}
+
+function getAnswer (score, db = connection) {
+  return db('answers')
+  .select()
+  .where ('score', score)
+}
