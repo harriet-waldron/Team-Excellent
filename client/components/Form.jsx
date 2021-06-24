ad70abd88c9551597bd2f5bcf04e0a052ad8f9c1
lucas-f0ster <lucasf284@gmail.com>
2021-06-25 10:08:17 +1200

    repair db install

--- client/components/Form.jsx
+++ client/components/Form.jsx
@@ -5,7 +5,14 @@ const Form = () => {
     <div className='form'>
       <form >
         <h3>This is the first question?</h3>
-        <input type='radio' name='question1' value='1'
+        <label htmlFor='q1-1'>1</label>
+        <input type='radio' name='question1' id='q1-1' value='1' />
+        <label htmlFor='q1-2'>1</label>
+        <input type='radio' name='question1' id='q1-2' value='2' />
+        <label htmlFor='q1-3'>1</label>
+        <input type='radio' name='question1' id='q1-3' value='3' />
+        <label htmlFor='q1-4'>1</label>
+        <input type='radio' name='question1' id='q1-4' value='4' />
       </form>
     </div>
   )
