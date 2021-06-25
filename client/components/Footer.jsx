47eb92e0f771f7122f104e2fb592113d854041a8
frederik schwarz <frederik@jens-schwarz.com>
2021-06-25 12:14:50 +1200

    made the body

--- client/components/Footer.jsx
+++ client/components/Footer.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='footer'>
-      <h1>footer</h1>
+    <div className='footerContainer'>
+    <img className='poOne' src='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.searchpng.com%2Fwp-content%2Fuploads%2F2019%2F03%2FKung-Fu-Panda-Transparent-PNG-Image-1024x1024.png&f=1&nofb=1'></img>
     </div>
   )
 }
--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -2,11 +2,15 @@ import React from 'react'
 
 const Header = () => {
   return (
+    <>
     <div className='headerContainer'>
     <div className='titleContainer'>
-      <h1 className='headerTitle'>Header</h1>
+      <h1 className='headerTitle'>Kung Fu</h1>
     </div>
     </div>
+    
+
+    </>
   )
 }
 
