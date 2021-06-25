0955fc9781a3b117442a0639b3523d5d07f18f24
lucas-f0ster <lucasf284@gmail.com>
2021-06-25 15:04:49 +1200

    done

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -5,32 +5,22 @@ import Form from './Form.jsx'
 import ExternalResponse from './ExternalResponse.jsx'
 import InternalResponse from './InternalResponse.jsx'
 import Footer from './Footer.jsx'
+import Landing from './Landing.jsx'
 import { externalApi, internalApi } from '../api'
 
 const App = () => {
   const [submitted, setSubmitted] = useState(false)
   const [info, setInfo] = useState({})
+  const [landing, setLanding] = useState(true)
 
   // this is where i do usestate
   const [horoscope, setHoroscope] = useState({})
-  const [answer, setAnswer] = useState({})
-
-  useEffect(() => {
-    if (info.score) {
-      internalApi(info.score)
-        .then((result) => {
-          // console.log('result :' ,result[0])
-          setAnswer(result[0])
-        })
-    }
-  }, [info])
 
   useEffect(() => {
     if (info.starSign) {
       externalApi(info.starSign)
-        .then((result) => {
-          setHoroscope(result)
-        })
+        .then((result) => setHoroscope(result))
+        .catch(err => console.log(err))
     }
   }, [info])
 
@@ -46,16 +36,23 @@ const App = () => {
 
   return (
     <div className='app'>
-
-      <Header />
-      {!submitted ? <Form update={(resp) => formResponse(resp)} />
+      {landing
+        ? <>
+          <Header />
+          <Landing ready={() => setLanding(false)}/>
+          <Footer />
+        </>
         : <>
+          <Header />
+          {!submitted ? <Form update={(resp) => formResponse(resp)} />
+            : <>
 
-          <ExternalResponse obj={horoscope}/>
-          <InternalResponse text={answer}/>
+              <ExternalResponse obj={horoscope} name={info.name} star={info.starSign} />
+              <InternalResponse text={'are you there'}/>
 
-        </>}
-      <Footer />
+            </>}
+          <Footer />
+        </> }
     </div>
   )
 }
