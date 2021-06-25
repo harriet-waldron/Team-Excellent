1207e9da5f36408179e480f6d805abe45eba6ce6
frederik schwarz <frederik@jens-schwarz.com>
2021-06-25 12:58:51 +1200

    added pandas

--- client/components/Footer.jsx
+++ client/components/Footer.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='footerContainer'>
-    <img className='poOne' src='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.searchpng.com%2Fwp-content%2Fuploads%2F2019%2F03%2FKung-Fu-Panda-Transparent-PNG-Image-1024x1024.png&f=1&nofb=1'></img>
+    <img className='PoOne' src='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.searchpng.com%2Fwp-content%2Fuploads%2F2019%2F03%2FKung-Fu-Panda-Transparent-PNG-Image-1024x1024.png&f=1&nofb=1'></img>
+    <img className='PoTwo' src='https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2F3.bp.blogspot.com%2F-HDiQwGP5qpA%2FT9lEdsOLJ1I%2FAAAAAAAAAOk%2FlHKh7LPwPDw%2Fs1600%2Fkung%2Bfu%2Bpanda%2B3.png&f=1&nofb=1'></img>
+    <img className='PoThree' src='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fmarketing-boot-camp.com%2Fwp-content%2Fuploads%2F2016%2F02%2Fkung-fu-panda-quotes.png&f=1&nofb=1'></img>
+    <img className='PoFour' src='https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2F4.bp.blogspot.com%2F-TAywLkkuqxs%2FT9eAA4MAQzI%2FAAAAAAAAGmw%2FgMuoDkMgVCo%2Fs1600%2FRender%2B-%2BKung%2BFu%2BPanda.png&f=1&nofb=1'></img>
     </div>
   )
 }
